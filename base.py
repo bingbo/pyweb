6d183276b8601cb1c3a6f9a4cbc75324307373a7
bingbo <zhangbingbingx@163.com>
2015-11-27 18:26:20 +0800

    update

--- base.py
+++ base.py
@@ -11,6 +11,8 @@
 '''
 
 import json
+import MySQLdb as mdb
+from config import db
 
 __all__ = [
     'BaseController' 
@@ -27,3 +29,29 @@ class BaseController:
         self._request = request
 
 
+class SqlClient:
+    '操作数据库蕨类'
+
+    __conn = None
+    __cursor = None
+
+    def __init__(self):
+        '构造函数，初始化工作'
+        self.__conn = mdb.connect(db['host'],db['username'],db['password'],db['database'],db['port'])
+        self.__cursor = self.__conn.cursor()
+        
+    def _select(self, table, fields, conds = None, appends = None):
+        '查询数据'
+        query = 'select %s from %s' % (', '.join(fields), table)
+        if conds:
+            query = query + ' where ' + 'and '.join(conds)
+        if appends:
+            query = query + ' ' + ' '.join(appends)
+
+        return self._execute(query)
+
+    def _execute(self, sql):
+        '原生执行sql'
+        self.__cursor.execute(sql)
+        rows  = self.__cursor.fetchall()
+        return rows
---
+++ config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+    @file config.py
+    @author bingbo(zhangbingbinge@126.com)
+    @date 2015/11/05 10:42:12
+    @brief configure info 
+'''
+
+__all__ = [
+    'db'
+]
+
+db = {
+    'database': 'wise_middle',
+    'host': '127.0.0.1',
+    'port': 3306,
+    'username': 'root',
+    'password': '123456'
+};
+
